95700599b654d4caf9f1d91bb3551050008cca4e
datletechxd <datletechxd@gmail.com>
2025-03-28 13:27:57 +0700

    Create Mod folder and MainMod.cs

---
+++ DragonBoy/Mod/MainMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonBoy.Mod
+{
+    internal class MainMod
+    {
+        public static MainMod _Instance;
+
+        public static MainMod getInstance()
+        {
+            if (_Instance == null)
+            {
+                _Instance = new MainMod();
+            }
+            return _Instance;
+        }
+    }
+}
